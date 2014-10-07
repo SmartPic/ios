6b795829d8a76be69ef3b811d64c380beb330304
tanabee <tanabee1986@gmail.com>
2014-10-07 22:32:44 +0900

    詳細ページのたたき作成

---
+++ SmartPic/Pages/Detail/DetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.swift
+//  SmartPic
+//
+//  Created by tanabe yuki on 2014/10/07.
+//  Copyright (c) 2014年 Yuki Tanabe. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var bigImageView: UIImageView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var pictures: [UIImage] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bigImageView.contentMode = .ScaleAspectFit
+        bigImageView.image = pictures[0]
+    }
+
+    @IBAction func tapOrderButton(sender: AnyObject) {
+        self.performSegueWithIdentifier("modalComplete", sender: nil)
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: ListImageCell = collectionView.dequeueReusableCellWithReuseIdentifier(ListImageCell.className, forIndexPath: indexPath) as ListImageCell
+        print(cell)
+        cell.listImageView.image = pictures[indexPath.row]
+        return cell
+    }
+}
