1ee788c989b5d60902b4ca64ad94909b4961462a
tanabee <tanabee1986@gmail.com>
2014-10-26 15:15:54 +0900

    デザインあてが終わっている部分のローカライズ対応完了

--- SmartPic/Pages/List/ListImageCell.swift
+++ SmartPic/Pages/List/ListImageCell.swift
@@ -13,8 +13,12 @@ class ListImageCell: UICollectionViewCell {
     @IBOutlet weak var listImageView: UIImageView!
     @IBOutlet weak var moreView: UIView!
     @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var photosLabel: UILabel!
     
     override func awakeFromNib() {
+        super.awakeFromNib()
         listImageView.contentMode = .ScaleAspectFill
+        photosLabel.text = NSLocalizedString("photos", comment: "")
+        
     }
 }
