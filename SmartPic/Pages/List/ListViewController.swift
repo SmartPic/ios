143958f4a2eea7ae64f810c0cb33873086201802
tanabee <tanabee1986@gmail.com>
2014-10-27 23:07:07 +0900

    バースト画像も表示されるよう修正

--- SmartPic/Pages/List/ListViewController.swift
+++ SmartPic/Pages/List/ListViewController.swift
@@ -210,7 +210,7 @@ class ListViewController: GAITrackedViewController, UITableViewDataSource, UITab
         alert.addAction(UIAlertAction(title: "設定画面へ", style: .Default, handler: { (action) -> Void in
             // 設定画面へ遷移する
             let url = NSURL(string: UIApplicationOpenSettingsURLString)
-            UIApplication.sharedApplication().openURL(url)
+            UIApplication.sharedApplication().openURL(url!)
         }))
         
         self.presentViewController(alert, animated: true, completion: nil)
