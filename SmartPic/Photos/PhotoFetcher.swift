1d778a47cc1e1f03051f9c3e44dbff5a38595e04
Ryosuke Hiramatsu <rhiramat@yahoo-corp.jp>
2014-11-05 22:09:18 +0900

    PhotoStreamの影響で写真が複数枚表示される不具合修正

--- SmartPic/Photos/PhotoFetcher.swift
+++ SmartPic/Photos/PhotoFetcher.swift
@@ -172,11 +172,27 @@ class PhotoFetcher: NSObject {
     }
     
     func allPhotoGroupingByTime() -> [GroupInfo] {
-        return groupingPhotos(false, isExceptICloud: false)
+        let iOS81 = NSOperatingSystemVersion(majorVersion: 8, minorVersion: 1, patchVersion: 0)
+        if NSProcessInfo().isOperatingSystemAtLeastVersion(iOS81) {
+            println("iOS 8.1 later")
+            return groupingPhotos(false, isExceptICloud: false)
+        }
+        else {
+            println("iOS 8.1 earlier")
+            return groupingPhotosWithMoments(false, isExceptICloud: false)
+        } 
     }
     
     func targetPhotoGroupingByTime() -> [GroupInfo] {
-        return groupingPhotos(true, isExceptICloud: false)
+        let iOS81 = NSOperatingSystemVersion(majorVersion: 8, minorVersion: 1, patchVersion: 0)
+        if NSProcessInfo().isOperatingSystemAtLeastVersion(iOS81) {
+            println("iOS 8.1 later")
+            return groupingPhotos(true, isExceptICloud: false)
+        }
+        else {
+            println("iOS 8.1 earlier")
+            return groupingPhotosWithMoments(true, isExceptICloud: false)
+        }
     }
     
     
