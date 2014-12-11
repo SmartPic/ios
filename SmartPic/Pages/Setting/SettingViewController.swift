bffac023a25bd3a572061d50434bd893045327cd
Ryosuke Hiramatsu <himaratsu@gmail.com>
2014-12-11 22:38:55 +0900

    設定画面を追加

---
+++ SmartPic/Pages/Setting/SettingViewController.swift
@@ -0,0 +1,76 @@
+//
+//  SettingViewController.swift
+//  SmartPic
+//
+//  Created by himara2 on 2014/12/11.
+//  Copyright (c) 2014年 Yuki Tanabe. All rights reserved.
+//
+
+import UIKit
+
+enum SettingType: Int {
+    case Request = 0, Review, Version
+    
+    static func allValues() -> [SettingType] {
+        return [Request, Review, Version]
+    }
+}
+
+class SettingViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    // MARK: - UITableViewDelegate 
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return SettingType.allValues().count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cellIdentifier = ""
+        let version = NSBundle.mainBundle().infoDictionary!["CFBundleShortVersionString"] as? String
+        
+        switch indexPath.row {
+        case SettingType.Request.rawValue:
+            cellIdentifier = "RequestCell"
+        case SettingType.Review.rawValue:
+            cellIdentifier = "ReviewCell"
+        case SettingType.Version.rawValue:
+            cellIdentifier = "VersionCell"
+        default:
+            cellIdentifier = ""
+        }
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as UITableViewCell
+        cell.detailTextLabel?.text = version
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 70
+    }
+
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        switch indexPath.row {
+        case SettingType.Request.rawValue:
+            Helpshift.sharedInstance().showConversation(self, withOptions: nil)
+        case SettingType.Review.rawValue:
+            UIApplication.sharedApplication().openURL(NSURL(string: kAppStoreUrl)!)
+        default:
+            println("do nothing")
+        }
+    }
+    
+    
+
+}
