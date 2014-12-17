1dabb136ebf36b7b0bd7122f8209862ea6fa2d43
tanabee <tanabee1986@gmail.com>
2014-12-17 20:31:56 +0900

    contentMode を storyboard で指定

--- SmartPic/Pages/List/CollectionImageCell.swift
+++ SmartPic/Pages/List/CollectionImageCell.swift
@@ -10,9 +10,4 @@ import UIKit
 
 class CollectionImageCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        imageView.contentMode = .ScaleAspectFill
-    }
 }
